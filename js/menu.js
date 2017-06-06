8a87bf5bd3e32bb9dc1d8e015b234f6c62041c57
James Screen <jamesscreen@yandex.ru>
2017-06-06 12:32:00 +0300

    add 780px menu correct

--- js/menu.js
+++ js/menu.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 $(window).load(function(){
 var lastId,
     topMenu = $(".menu__ul"),
-    topMenuHeight = topMenu.outerHeight()+70,
+    topMenuHeight = topMenu.outerHeight()+72,
     // All list items
     menuItems = topMenu.find("a"),
     // Anchors corresponding to menu items
